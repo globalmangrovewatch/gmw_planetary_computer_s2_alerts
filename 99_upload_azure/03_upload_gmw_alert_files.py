ad01c6b1eb70ffc033153d72857bb826e6299b19
Pete Bunting <petebunting@mac.com>
2023-01-24 18:40:46 +0000

    Script to upload the alert images.

---
+++ 99_upload_azure/03_upload_gmw_alert_files.py
@@ -0,0 +1,22 @@
+from azure.storage.blob import BlobClient
+import glob
+import os
+import json
+import tqdm
+
+sas_info_file = "/home/jovyan/azure_info.json"
+
+with open(sas_info_file) as f:
+    sas_token_info = json.load(f)
+
+gmw_tif_tiles = glob.glob("/home/jovyan/PlanetaryComputerExamples/gmw_alerts_plantary_computer_tests/monthly_change_imgs_v8/*.tif")
+
+for img_file in tqdm.tqdm(gmw_tif_tiles):
+    basename = os.path.basename(img_file)
+    file_url = os.path.join(sas_token_info["url"], "monthly_change_imgs", basename)
+    file_url_signed = f"{file_url}?{sas_token_info['sas_token']}"
+    #print(file_url_signed)
+    client = BlobClient.from_blob_url(file_url_signed)
+    with open(img_file, 'rb') as data:
+        client.upload_blob(data)
+    #print("\tFile Uploaded")
